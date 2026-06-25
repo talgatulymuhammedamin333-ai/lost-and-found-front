e0db81c2c9bc3fced885318e2a1d327e447d3720
Muhammedmain <talgatulymuhammedamin333@gmail.com>
2026-06-25 14:37:15 +0500

    dwmawd

--- src/widgets/header/Logo.jsx
+++ src/widgets/header/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <div className='flex'>
         <div className='container flex items-center w-10'>
-        <img className=' w-full h-full object-contain' src="logo.svg" alt="" />
+        <img className=' w-full h-full object-contain' src="/logo.svg" alt="" />
         </div>
         <h1 className='text-3xl font-bold'>Lost and Found</h1>
     </div>
